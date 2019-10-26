30a4f4ca3826ab22222961489f09658e82330aca
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-10-26 11:13:59 -0400

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Fulcherberguer, Fernanda on 2019-10-26.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
